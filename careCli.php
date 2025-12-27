b60dc1364d207087c399995af80815f91cacd015
youssef faouzi <yj1984515@gmail.com>
2025-12-27 14:00:18 +0100

    feat : Add doctor management

--- careCli.php
+++ careCli.php
@@ -7,7 +7,7 @@ class Database {
     public function __construct(){
         try {
             $this->pdo = new PDO(
-                "mysql:host=localhost;dbname=clinic_full;charset=utf8",
+                "mysql:host=localhost;dbname=CARE_CLINIC_MANAGMENT_CLI;charset=utf8",
                 "root",
                 ""
             );
@@ -19,18 +19,51 @@ class Database {
 }
 
 
+class basemodel{
+     protected $pdo;
+
+     public function __construct($pdo){
+        $this->pdo = $pdo ; 
+     }
+     
+}
+class doctor extends basemodel{
+    
+    public function create($na,$spe,$depID){
+         $this->pdo->prepare(
+            "INSERT INTO doctors (name,specialty,department_id) VALUES (?,?,?)"
+         )->execute([$na,$spe,$depID]);
+    }
+}
+
+$database = new database() ;
+$doctor = new doctor($database->pdo);
+// $doctor = new doctor();
+
 
 while(true){
     echo "***************menu*****************\n";
-    echo "1-doctors\n2-patients\n0-exit\nchose :" ;
+    echo "1-doctors\n2-patients\n3-departements\n0-exit\nchose :" ;
     $main = trim(fgets(STDIN));
     if($main=="1"){
          echo "***********doctors***********\n";
          echo "1-add doctor\n2-show doctor\n3-update doctor\n4-delet doctor\n0-exit\nchose:";
          $chosedoctor = trim(fgets(STDIN));
+         if($chosedoctor=="1"){
+            echo "name : "  ; $name = trim(fgets(STDIN));
+            echo "specyalite : " ; $spec =   trim(fgets(STDIN));
+            echo "departement ID : "  ; $depID =  trim(fgets(STDIN));
+
+            $doctor->create($name,$spec,$depID);
+            
+         }
     }elseif($main=="2"){
          echo "***********patients**********\n";
          echo "1-add patient\n2-show patient\n3-update patient\n4-delet patient\n0-exit\nchose:";
          $chosepatient = trim(fgets(STDIN));
+    }elseif($main=="3"){
+         echo "***********departements**********\n";
+         echo "1-add departement\n2-show departement\n3-update departement\n4-delet departement\n0-exit\nchose:";
+         $chosedepartement = trim(fgets(STDIN));
     }
 }
